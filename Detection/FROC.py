c31b9d4a498cbdaf6966331b6e3e6b3cdd300fd5
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-06 16:28:03 -0400

    Update FROC.py

--- Detection/FROC.py
+++ Detection/FROC.py
@@ -1,40 +1,69 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
-def count_tp_fp_fn(pred,gt,score,distance_threshold):   
+def count_tp_fp_fn(pred,gt,prob_threshold,hit_distance):   
     if pred.shape[0]>0:
-        pred = pred[np.argwhere(pred[:,2]>=score)[:,0],:2]
-        if gt.shape[0]>0 and pred.shape[0]>0:  
+        pred = pred[np.argwhere(pred[:,2]>=prob_threshold)[:,0],:2]        
+    if pred.shape[0]>0:    
+        if gt.shape[0]>0:
             hit = cdist(pred[:,:2],gt)
-            hit[hit<=distance_threshold] = 1
+            hit[hit<=hit_distance] = 1
             hit[hit!=1] = 0        
             sum_1 = np.sum(hit,0)
             sum_2 = np.sum(hit,1)
-            false_positives = sum(sum_2 ==0)
-            false_negatives = sum(sum_1 ==0)
-            true_positives = sum(sum_1 !=0)
+            fp = sum(sum_2 ==0)
+            fn = sum(sum_1 ==0)
+            tp = sum(sum_1 !=0)
         else:
-            true_positives = 0
-            false_negatives = 0
-            false_positives = pred.shape[0]
+            tp = 0
+            fn = 0
+            fp = pred.shape[0]
     else:
-        true_positives = 0
-        false_negatives = 0
-        false_positives = pred.shape[0]
-    return true_positives, false_positives, false_negatives
+        tp = 0
+        fp = 0
+        fn = gt.shape[0]
+    return tp, fp, fn
 
-def FROC(pred,gt,confidence_thresholds,distance_threshold):
+######## Calculating TP,FP,FN for each Patch, Add them all and then calculate Sen,FP-Per-Patch
+
+def FROC(pred,gt,confidence_thresholds,hit_distance):
     sens = []
     fp_img = []
-    for i in confidence_thresholds:        
-        print(i)
-        sensitivity = []
-        fp_per_image = []        
+    for threshold in confidence_thresholds:
+        tps = []
+        fps = []
+        fns = []
         for N in range(len(gt)):            
-            true_positives, false_positives, false_negatives = count_tp_fp_fn(pred[N],gt[N],i,distance_threshold)
-            if true_positives!=0:            
-                sensitivity.append(true_positives/(true_positives+false_negatives))
-            fp_per_image.append(false_positives)
-        sens.append(np.mean(sensitivity))
-        fp_img.append(np.mean(fp_per_image))
+            tp, fp, fn = count_tp_fp_fn(pred[N],gt[N],threshold,hit_distance)
+            tps.append(tp)
+            fps.append(fp)
+            fns.append(fn)
+        tps = sum(tps)
+        fp_per_img = np.mean(fps)
+        fps = sum(fps)
+        fns = sum(fns)
+        if tps!=0:
+            sens.append(tps/(tps+fns))
+        else:
+            sens.append(0)
+        fp_img.append(fp_per_img)
     return sens, fp_img
+
+######## Calculating Sens,FP_per_Image Patch Wise and then taking the average
+
+# def FROC(pred,gt,confidence_thresholds,hit_distance):
+#     sens = []
+#     fp_img = []
+#     for threshold in confidence_thresholds:        
+#         sensitivity = []
+#         fp_per_image = [] 
+#         for N in range(len(gt)):            
+#             tp, fp, fn = count_tp_fp_fn(pred[N],gt[N],threshold,hit_distance)
+#             if tp!=0:            
+#                 sensitivity.append(tp/(tp+fn))
+#             if tp==0 and fp!=0 or fn!=0:
+#                 sensitivity.append(0)                     
+#             fp_per_image.append(fp)
+#         sens.append(np.mean(sensitivity))
+#         fp_img.append(np.mean(fp_per_image))
+#     return sens, fp_img
