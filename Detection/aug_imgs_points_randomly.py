300ecd851fff750bb88af66a14066491374ed032
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-12 12:10:37 -0400

    Update aug_imgs_points_randomly.py

--- Detection/aug_imgs_points_randomly.py
+++ Detection/aug_imgs_points_randomly.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def aug_imgs_points_randomly(patches, patches_points, patch_size = 128):
+    # this function randomly flips patches and detections by keeping the original patches, flipping patches left-right, flipping patches up-down, and transposing patches.
+    
+    # inputs: patches = a list of patches images: [patch_1, patch_2, patch_3, ....]
+    # patches_points = a list of TILs for patches: [points_1, points_2, points_3, ...]
+    # patch_1 : numpy array of shape(patch_size,patch_size,3)
+    # point_1 : numpy array of shape (N,2): N number of TILs.
+    # patch_size = size of the patch images.
+    
+    # outputs: list of randomly flipped patches and points.
+    
     idx = np.random.RandomState(42).randint(0,4,len(patches))    
     aug_patches = []
     aug_patches_points = []    
