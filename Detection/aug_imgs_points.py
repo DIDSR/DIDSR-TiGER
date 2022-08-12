85e8b25ef746abdc294bb07d2df555ab196bf158
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-12 12:09:11 -0400

    Update aug_imgs_points.py

--- Detection/aug_imgs_points.py
+++ Detection/aug_imgs_points.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def aug_imgs_points(patches, patches_points, patch_size = 128):
+    # this function augments patches and detections by keeping the original patches, flipping patches left-right, flipping patches up-down, and transposing patches.
+    
+    # inputs: patches = a list of patches images: [patch_1, patch_2, patch_3, ....]
+    # patches_points = a list of TILs for patches: [points_1, points_2, points_3, ...]
+    # patch_1 : numpy array of shape(patch_size,patch_size,3)
+    # point_1 : numpy array of shape (N,2): N number of TILs.
+    # patch_size = size of the patch images.
+    
+    # outputs: list of augmneted patches and points. Increased 4 times.
     aug_patches = []
     aug_patches_points = []    
     for i in range(len(patches)):        
