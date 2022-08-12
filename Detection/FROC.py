73be4f4929e4c0bd15ee40ca1dc5bb680730fc63
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-12 12:01:41 -0400

    Update FROC.py

--- Detection/FROC.py
+++ Detection/FROC.py
@@ -1,7 +1,16 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
-def count_tp_fp_fn(pred,gt,prob_threshold,hit_distance):   
+def count_tp_fp_fn(pred,gt,prob_threshold,hit_distance): 
+    # This function counts the number of true-positives, false negatives and false positives in a detection taks. 
+    
+    # inputs: A numpy array of shape [N,3] for pred and gt. N is the number of detections. The first column is the x-position of the detections.
+    # the second column is the y-position of the detections. the third column is the probability associated with detections.
+    # The hit-distance is the minimum distance by which if a detection is within the hit-distance radius of the ground truth, it will be counted as a true-positive,
+    # otherwsie, it will be counted as a false positive.
+    # The prob_threshld is the threshold value in which detections with probabilities smaller than the threshold value will be discarded.
+    
+    #output: number of ture-positives, false positives, and false negatives.
     if pred.shape[0]>0:
         pred = pred[np.argwhere(pred[:,2]>=prob_threshold)[:,0],:2]        
     if pred.shape[0]>0:    
@@ -27,7 +36,19 @@ def count_tp_fp_fn(pred,gt,prob_threshold,hit_distance):
 ######## Calculating TP,FP,FN for each Patch, Add them all and then calculate Sen,FP-Per-Patch
 
 def FROC(pred,gt,confidence_thresholds,hit_distance):
+    # this function calculates the average sensitivity and average number of fp_per_image.
+    
+    # inputs: pred and gt are lists containing detections from the ground truth and the predictions from the model. for example:
+    # pred = [pred_1, pred_2, pred_3], gt = [gt_1, gt_2, gt_3]
+    # pred_1 = np.array((N,3))  pred_2 = np.array((M,3))    pred_3 = np.array((P,3))
+    # gt_1 = np.array((N,3))    gt_2 = np.array((M,3))      gt_3 = np.array((P,3))    
+    # confidence_thresholds is the list of the probability thresholds in calculating the FROC curve. for example:
+    # confidence_thresholds = np.linspace(0,1,40)    
+    # hit-distance: is the maximum distance from the ground truth by which a detection will be counted as a true positive.
+    
+    #outputs: returns a list for sensitivies and fps_per_image for each of the threshold values.
     sens = []
+    
     fp_img = []
     for threshold in confidence_thresholds:
         tps = []
