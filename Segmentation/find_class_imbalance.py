926d741ce3ab9dbff7ed584d17af738fafeefc50
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-15 08:25:19 -0400

    Update find_class_imbalance.py

--- Segmentation/find_class_imbalance.py
+++ Segmentation/find_class_imbalance.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def find_class_imbalance(masks):    
+def find_class_imbalance(masks):   
+    # This function counts the number of pixels in a segmentation mask and returns the percentage of the counts for each class.
+    
+    # Input: masks is a list containing the segmentation masks. 
+    # Output: returns a list containing the percentage or class imbalance between the classes in the segmentation masks.
     unique_vals = [np.unique(i, return_counts = True) for i in masks]
     
     classes = np.unique(np.concatenate([i[0] for i in unique_vals]))
