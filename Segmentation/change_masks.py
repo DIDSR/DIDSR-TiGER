3d326f2520237bec53283b142b8afe4df599fd4e
Arian Arab <68286434+arian-arab@users.noreply.github.com>
2022-08-15 08:22:27 -0400

    Update change_masks.py

--- Segmentation/change_masks.py
+++ Segmentation/change_masks.py
@@ -1,4 +1,6 @@
 def change_masks(input_data):    
+    # This function relables the input_mask to segment the tissue into "rest", "tumor", and "stroma" classes.
+    # input_data: numpy array of size (N,M)
     input_data[input_data == 3] = 0
     input_data[input_data == 4] = 0
     input_data[input_data == 5] = 0
